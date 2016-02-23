6fefe4edb8251f9cff11b02acdc754714e9da17e
wayne.workman2012 <wayne.workman2012@gmail.com>
2016-02-23 00:17:32 -0600

    Further organized files.

---
+++ old/ad.php
@@ -0,0 +1,127 @@
+<?php
+
+
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+include 'vars.php';
+
+// Create connection
+$link = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($link->connect_error) die("Connection failed: $link->connect_error");
+
+// Escape user inputs for security
+
+$SettingsNickName = $link->real_escape_string($_REQUEST['SettingsNickName']);
+$OldSettingsNickName = $SettingsNickName;
+$SettingsPassword = $link->real_escape_string($_REQUEST['SettingsPassword']);
+$Name = $link->real_escape_string($_REQUEST['Name']);
+$AccountExpirationDate = $link->real_escape_string($_REQUEST['AccountExpirationDate']);
+$AccountNotDelegated = $link->real_escape_string($_REQUEST['AccountNotDelegated']);
+$AccountPassword = $link->real_escape_string($_REQUEST['AccountPassword']);
+$Action = $link->real_escape_string($_REQUEST['Action']);
+$AllowReversiblePasswordEncryption = $link->real_escape_string($_REQUEST['AllowReversiblePasswordEncryption']);
+$AuthType = $link->real_escape_string($_REQUEST['AuthType']);
+$CannotChangePassword = $link->real_escape_string($_REQUEST['CannotChangePassword']);
+$ChangePasswordAtLogon = $link->real_escape_string($_REQUEST['ChangePasswordAtLogon']);
+$City = $link->real_escape_string($_REQUEST['City']);
+$Company = $link->real_escape_string($_REQUEST['Company']);
+$Country = $link->real_escape_string($_REQUEST['Country']);
+$Credential  = $link->real_escape_string($_REQUEST['Credential']);
+$Certificates = $link->real_escape_string($_REQUEST['Certificates']);
+$Department = $link->real_escape_string($_REQUEST['Department']);
+$Description = $link->real_escape_string($_REQUEST['Description']);
+$DisplayName = $link->real_escape_string($_REQUEST['DisplayName']);
+$Division = $link->real_escape_string($_REQUEST['Division']);
+$EmailAddress = $link->real_escape_string($_REQUEST['EmailAddress']);
+$EmployeeID = $link->real_escape_string($_REQUEST['EmployeeID']);
+$EmployeeNumber = $link->real_escape_string($_REQUEST['EmployeeNumber']);
+$Enabled = $link->real_escape_string($_REQUEST['Enabled']);
+$Fax = $link->real_escape_string($_REQUEST['Fax']);
+$GivenName = $link->real_escape_string($_REQUEST['GivenName']);
+$HomeDirectory = $link->real_escape_string($_REQUEST['HomeDirectory']);
+$HomeDrive = $link->real_escape_string($_REQUEST['HomeDrive']);
+$HomePage = $link->real_escape_string($_REQUEST['HomePage']);
+$HomePhone = $link->real_escape_string($_REQUEST['HomePhone']);
+$Initials = $link->real_escape_string($_REQUEST['Initials']);
+$Instance = $link->real_escape_string($_REQUEST['Instance']);
+$LogonWorkstations = $link->real_escape_string($_REQUEST['LogonWorkstations']);
+$Manager = $link->real_escape_string($_REQUEST['Manager']);
+$MobilePhone = $link->real_escape_string($_REQUEST['MobilePhone']);
+$Office = $link->real_escape_string($_REQUEST['Office']);
+$OfficePhone = $link->real_escape_string($_REQUEST['OfficePhone']);
+$Organization = $link->real_escape_string($_REQUEST['Organization']);
+$OtherAttributes = $link->real_escape_string($_REQUEST['OtherAttributes']);
+$OtherName = $link->real_escape_string($_REQUEST['OtherName']);
+$PassThru = $link->real_escape_string($_REQUEST['PassThru']);
+$PasswordNeverExpires = $link->real_escape_string($_REQUEST['PasswordNeverExpires']);
+$PasswordNotRequired = $link->real_escape_string($_REQUEST['PasswordNotRequired']);
+$Path = $link->real_escape_string($_REQUEST['Path']);
+$POBox = $link->real_escape_string($_REQUEST['POBox']);
+$PostalCode = $link->real_escape_string($_REQUEST['PostalCode']);
+$ProfilePath = $link->real_escape_string($_REQUEST['ProfilePath']);
+$SamAccountName = $link->real_escape_string($_REQUEST['SamAccountName']);
+$ScriptPath = $link->real_escape_string($_REQUEST['ScriptPath']);
+$Server = $link->real_escape_string($_REQUEST['Server']);
+$ServicePrincipalNames = $link->real_escape_string($_REQUEST['ServicePrincipalNames']);
+$SmartcardLogonRequired = $link->real_escape_string($_REQUEST['SmartcardLogonRequired']);
+$State = $link->real_escape_string($_REQUEST['State']);
+$StreetAddress = $link->real_escape_string($_REQUEST['StreetAddress']);
+$Surname = $link->real_escape_string($_REQUEST['Surname']);
+$Title = $link->real_escape_string($_REQUEST['Title']);
+$TrustedForDelegation = $link->real_escape_string($_REQUEST['TrustedForDelegation']);
+$Type = $link->real_escape_string($_REQUEST['Type']);
+$UserPrincipalName = $link->real_escape_string($_REQUEST['UserPrincipalName']);
+$Confirm = $link->real_escape_string($_REQUEST['Confirm']);
+$WhatIf = $link->real_escape_string($_REQUEST['WhatIf']);
+
+
+
+
+
+// attempt query execution
+
+
+switch ($Action) {
+
+case "new":
+
+// Encrypt password
+$SettingsPassword = password_hash("$SettingsPassword", PASSWORD_DEFAULT);
+
+
+
+$sql = "INSERT INTO `janeAD` (`SettingsNickName`, `SettingsPassword`, `Name`, `AccountExpirationDate`, `AccountNotDelegated`, `AccountPassword`, `AllowReversiblePasswordEncryption`, `AuthType`, `CannotChangePassword`, `Certificates`, `ChangePasswordAtLogon`, `City`, `Company`, `Country`, `Credential`, `Department`, `Description`, `DisplayName`, `Division`, `EmailAddress`, `EmployeeID`, `EmployeeNumber`, `Enabled`, `Fax`, `GivenName`, `HomeDirectory`, `HomeDrive`, `HomePage`, `HomePhone`, `Initials`, `Instance`, `LogonWorkstations`, `Manager`, `MobilePhone`, `Office`, `OfficePhone`, `Organization`, `OtherAttributes`, `OtherName`, `PassThru`, `PasswordNeverExpires`, `PasswordNotRequired`, `Path`, `POBox`, `PostalCode`, `ProfilePath`, `SamAccountName`, `ScriptPath`, `Server`, `ServicePrincipalNames`, `SmartcardLogonRequired`, `State`, `StreetAddress`, `Surname`, `Title`, `TrustedForDelegation`, `Type`, `UserPrincipalName`, `Confirm`, `WhatIf`) VALUES ('$SettingsNickName','$SettingsPassword','$Name','$AccountExpirationDate','$AccountNotDelegated','$AccountPassword','$AllowReversiblePasswordEncryption','$AuthType','$CannotChangePassword','$Certificates','$ChangePasswordAtLogon','$City','$Company','$Country','$Credential','$Department','$Description','$DisplayName','$Division','$EmailAddress','$EmployeeID','$EmployeeNumber','$Enabled','$Fax','$GivenName','$HomeDirectory','$HomeDrive','$HomePage','$HomePhone','$Initials','$Instance','$LogonWorkstations','$Manager','$MobilePhone','$Office','$OfficePhone','$Organization','$OtherAttributes','$OtherName','$PassThru','$PasswordNeverExpires','$PasswordNotRequired','$Path','$POBox','$PostalCode','$ProfilePath','$SamAccountName','$ScriptPath','$Server','$ServicePrincipalNames','$SmartcardLogonRequired','$State','$StreetAddress','$Surname','$Title','$TrustedForDelegation','$Type','$UserPrincipalName','$Confirm','$WhatIf');";
+break;
+
+case "edit":
+
+
+
+
+// Updating
+
+$sql = "UPDATE `janeAD` SET `SettingsNickName`='$SettingsNickName', `SettingsPassword`='$SettingsPassword', `Name`='$Name', `AccountExpirationDate`='$AccountExpirationDate', `AccountNotDelegated`='$AccountNotDelegated', `AccountPassword`='$AccountPassword', `AllowReversiblePasswordEncryption`='$AllowReversiblePasswordEncryption', `AuthType`='$AuthType', `CannotChangePassword`='$CannotChangePassword', `Certificates`='$Certificates', `ChangePasswordAtLogon`='$ChangePasswordAtLogon', `City`='$City', `Company`='$Company', `Country`='$Country', `Credential`='$Credential', `Department`='$Department', `Description`='$Description', `DisplayName`='$DisplayName', `Division`='$Division', `EmailAddress`='$EmailAddress', `EmployeeID`='$EmployeeID', `EmployeeNumber`='$EmployeeNumber', `Enabled`='$Enabled', `Fax`='$Fax', `GivenName`='$GivenName', `HomeDirectory`='$HomeDirectory', `HomeDrive`='$HomeDrive', `HomePage`='$HomePage', `HomePhone`='$HomePhone', `Initials`='$Initials', `Instance`='$Instance', `LogonWorkstations`='$LogonWorkstations', `Manager`='$Manager', `MobilePhone`='$MobilePhone', `Office`='$Office', `OfficePhone`='$OfficePhone', `Organization`='$Organization', `OtherAttributes`='$OtherAttributes', `OtherName`='$OtherName', `PassThru`='$PassThru', `PasswordNeverExpires`='$PasswordNeverExpires', `PasswordNotRequired`='$PasswordNotRequired', `Path`='$Path', `POBox`='$POBox', `PostalCode`='$PostalCode', `ProfilePath`='$ProfilePath', `SamAccountName`='$SamAccountName', `ScriptPath`='$ScriptPath', `Server`='$Server', `ServicePrincipalNames`='$ServicePrincipalNames', `SmartcardLogonRequired`='$SmartcardLogonRequired', `State`='$State', `StreetAddress`='$StreetAddress', `Surname`='$Surname', `Title`='$Title', `TrustedForDelegation`='$TrustedForDelegation', `Type`='$Type', `UserPrincipalName`='$UserPrincipalName', `Confirm`='$Confirm', `WhatIf`='$WhatIf' WHERE `SettingsNickName` = '$OldSettingsNickName';";
+
+
+
+break;
+default:
+echo "Unidentified action.";
+}
+
+
+
+if ($link->query($sql)) {
+    echo 'Records added successfully.';
+    $link->close();
+} else {
+    $link->close();
+    die ("ERROR: Could not execute sql:\nStatement: \"$sql\"\nError: $link->error_list");
+}
+echo '<p><p><p>';
+echo '<a href="jane.php">Return</a>';
+
+?>
+
---
+++ old/doAction.php
@@ -0,0 +1,212 @@
+<?php
+include 'vars.php';
+// Escape user inputs for security
+
+$SettingsNickName = mysql_real_escape_string($_REQUEST['SettingsNickName']);
+$SettingsPassword = mysql_real_escape_string($_REQUEST['SettingsPassword']);
+$Action = mysql_real_escape_string($_REQUEST['Action']);
+$NewSettingType = mysql_real_escape_string($_REQUEST['NewSettingType']);
+
+
+
+
+
+
+switch ($Action) {
+
+case "edit":
+
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+
+
+// Create connection
+$link = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($link->connect_error) die("Connection failed: $link->connect_error");
+
+$sql = "SELECT SettingsPassword FROM janeAD WHERE SettingsNickName = '$SettingsNickName' LIMIT 1";
+
+$result = $link->query($sql);
+$StoredPassword = "";
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $StoredPassword = $row["SettingsPassword"];
+    }
+} else {
+    echo "0 results";
+}
+
+
+
+if (password_verify($SettingsPassword, $StoredPassword)) {
+$SettingsPassword = password_hash("$SettingsPassword", PASSWORD_DEFAULT);
+
+
+
+$sql = "SELECT * FROM janeAD WHERE SettingsNickName = '$SettingsNickName' LIMIT 1";
+
+$result = $link->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        
+	
+	$Name = $row["Name"];
+	$AccountExpirationDate = $row["AccountExpirationDate"];
+	$AccountNotDelegated = $row["AccountNotDelegated"];
+	$AccountPassword = $row["AccountPassword"];
+	$AllowReversiblePasswordEncryption = $row["AllowReversiblePasswordEncryption"];
+	$AuthType = $row["AuthType"];
+	$CannotChangePassword = $row["CannotChangePassword"];
+	$Certificates = $row["Certificates"];
+	$ChangePasswordAtLogon = $row["ChangePasswordAtLogon"];
+	$City = $row["City"];
+	$Company = $row["Company"];
+	$Country = $row["Country"];
+	$Credential = $row["Credential"];
+	$Department = $row["Department"];
+	$Description = $row["Description"];
+	$DisplayName = $row["DisplayName"];
+	$Division = $row["Division"];
+	$EmailAddress = $row["EmailAddress"];
+	$EmployeeID = $row["EmployeeID"];
+	$EmployeeNumber = $row["EmployeeNumber"];
+	$Enabled = $row["Enabled"];
+	$Fax = $row["Fax"];
+	$GivenName = $row["GivenName"];
+	$HomeDirectory = $row["HomeDirectory"];
+	$HomeDrive = $row["HomeDrive"];
+	$HomePage = $row["HomePage"];
+	$HomePhone = $row["HomePhone"];
+	$Initials = $row["Initials"];
+	$Instance = $row["Instance"];
+	$LogonWorkstations = $row["LogonWorkstations"];
+	$Manager = $row["Manager"];
+	$MobilePhone = $row["MobilePhone"];
+	$Office = $row["Office"];
+	$OfficePhone = $row["OfficePhone"];
+	$Organization = $row["Organization"];
+	$OtherAttributes = $row["OtherAttributes"];
+	$OtherName = $row["OtherName"];
+	$PassThru = $row["PassThru"];
+	$PasswordNeverExpires = $row["PasswordNeverExpires"];
+	$PasswordNotRequired = $row["PasswordNotRequired"];
+	$Path = $row["Path"];
+	$POBox = $row["POBox"];
+	$PostalCode = $row["PostalCode"];
+	$ProfilePath = $row["ProfilePath"];
+	$SamAccountName = $row["SamAccountName"];
+	$ScriptPath = $row["ScriptPath"];
+	$Server = $row["Server"];
+	$ServicePrincipalNames = $row["ServicePrincipalNames"];
+	$SettingsWHERE = $row["SettingsWHERE"];
+	$SmartcardLogonRequired = $row["SmartcardLogonRequired"];
+	$State = $row["State"];
+	$StreetAddress = $row["StreetAddress"];
+	$Surname = $row["Surname"];
+	$Title = $row["Title"];
+	$TrustedForDelegation = $row["TrustedForDelegation"];
+	$Type = $row["Type"];
+	$UserPrincipalName = $row["UserPrincipalName"];
+	$Confirm = $row["Confirm"];
+	$WhatIf = $row["WhatIf"];
+   	}
+$link->close();
+
+
+$NextURL = "adSetSettings.php?SettingsNickName=$SettingsNickName&SettingsPassword=$SettingsPassword&Name=$Name&AccountExpirationDate=$AccountExpirationDate&AccountNotDelegated=$AccountNotDelegated&AccountPassword=$AccountPassword&Action=$Action&AllowReversiblePasswordEncryption=$AllowReversiblePasswordEncryption&AuthType=$AuthType&CannotChangePassword=$CannotChangePassword&Certificates=$Certificates&ChangePasswordAtLogon=$ChangePasswordAtLogon&City=$City&Company=$Company&Country=$Country&Credential=$Credential&Department=$Department&Description=$Description&DisplayName=$DisplayName&Division=$Division&EmailAddress=$EmailAddress&EmployeeID=$EmployeeID&EmployeeNumber=$EmployeeNumber&Enabled=$Enabled&Fax=$Fax&GivenName=$GivenName&HomeDirectory=$HomeDirectory&HomeDrive=$HomeDrive&HomePage=$HomePage&HomePhone=$HomePhone&Initials=$Initials&Instance=$Instance&LogonWorkstations=$LogonWorkstations&Manager=$Manager&MobilePhone=$MobilePhone&Office=$Office&OfficePhone=$OfficePhone&Organization=$Organization&OtherAttributes=$OtherAttributes&OtherName=$OtherName&PassThru=$PassThru&PasswordNeverExpires=$PasswordNeverExpires&PasswordNotRequired=$PasswordNotRequired&Path=$Path&POBox=$POBox&PostalCode=$PostalCode&ProfilePath=$ProfilePath&SamAccountName=$SamAccountName&ScriptPath=$ScriptPath&Server=$Server&ServicePrincipalNames=$ServicePrincipalNames&SettingsWHERE=$SettingsWHERE&SmartcardLogonRequired=$SmartcardLogonRequired&State=$State&StreetAddress=$StreetAddress&Surname=$Surname&Title=$Title&TrustedForDelegation=$TrustedForDelegation&Type=$Type&UserPrincipalName=$UserPrincipalName&Confirm=$Confirm&WhatIf=$WhatIf";
+
+header("Location: $NextURL");
+
+	
+} else {
+    echo "0 results";
+$link->close();
+}
+
+
+
+} else {
+
+echo "Passwords_Do_Not_Match";
+$link->close();
+}
+
+
+break;
+
+case "delete":
+
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+
+
+
+
+// Create connection
+$link = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($link->connect_error) die("Connection failed: $link->connect_error");
+
+$sql = "SELECT SettingsPassword FROM janeAD WHERE SettingsNickName = '$SettingsNickName' LIMIT 1";
+
+$result = $link->query($sql);
+$StoredPassword = "";
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $StoredPassword = $row["SettingsPassword"];
+    }
+} else {
+    echo "0 results";
+}
+
+
+if (password_verify($SettingsPassword, $StoredPassword)) {
+
+
+$sql = "DELETE FROM janeAD WHERE SettingsNickName = '$SettingsNickName'";
+
+if ($link->query($sql) === TRUE) {
+    echo "Record deleted successfully<br>";
+	echo '<a href="jane.php">Return</a>';
+
+} else {
+    echo "Error deleting record: " . $link->error;
+}
+
+$link->close();
+
+
+} else {
+echo "Passwords don't match.";
+}
+
+
+
+
+break;
+case "new":
+
+$NextURL = "adSetSettings.php?Action=$Action";
+
+header("Location: $NextURL");
+
+break;
+default:
+echo "Something has gone wrong. Options were not passed correctly from jane.php";
+
+
+break;
+}
+
+
+?>
+
