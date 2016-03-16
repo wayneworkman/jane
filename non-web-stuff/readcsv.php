cc3a9f7d7b2d0335980a4bcc8c63f3a9f52ecab9
wayne.workman <wayne.workman2012@gmail.com>
2016-03-16 11:51:17 -0500

    Created a php-based script to handle reading from the CSV file due to punctuations in names causing issues. PHP has better handles for this stuff.

---
+++ non-web-stuff/readcsv.php
@@ -0,0 +1,50 @@
+<?php
+include 'localVars.php';
+include 'connect2db.php';
+
+// $PathToCSV
+
+
+$symbols = array();
+$symbols += range('a', 'z');
+$symbols += range('A', 'Z');
+$symbols += range('0', '9');
+array_push($symbols,' ','-');
+
+
+$csv = fopen($PathToCSV,'r') or die("can't open file");
+while($csv_line = fgetcsv($csv)) {
+	list($userAction, $userImportedID, $userFirstName, $userMiddleName, $userLastName, $userPassword, $userGroup) = $csv_line;
+	$userAction = preg_replace("/[^" . preg_quote(implode('',$symbols), '/') . "]/i", "", $userAction);
+	
+	// For some reason, this line throws out the entire string.
+	//$userImportedID = preg_replace("/[^" . preg_quote(implode('',$symbols), '/') . "]/i", "", $userImportedID);
+	
+	$userFirstName = preg_replace("/[^" . preg_quote(implode('',$symbols), '/') . "]/i", "", $userFirstName);
+	$userMiddleName = preg_replace("/[^" . preg_quote(implode('',$symbols), '/') . "]/i", "", $userMiddleName);
+	$userLastName = preg_replace("/[^" . preg_quote(implode('',$symbols), '/') . "]/i", "", $userLastName);
+	$userPassword = preg_replace("/[^" . preg_quote(implode('',$symbols), '/') . "]/i", "", $userPassword);
+	$userGroup = preg_replace("/[^" . preg_quote(implode('',$symbols), '/') . "]/i", "", $userGroup);
+
+
+	$userUserName = substr($userFirstName, 0, 1) . substr($userMiddleName,0,1) . substr($userLastName,0,1) . substr($userImportedID,5,4);
+	$userUserName = strtolower($userUserName);
+
+
+	$sql="INSERT INTO userDataToImport (userAction,userFirstName,userMiddleName,userLastName,userGroup,userUserName,userPassword,userImportedID) VALUES ('$userAction','$userFirstName','$userMiddleName','$userLastName','$userGroup','$userUserName','$userPassword','$userImportedID')";
+
+
+	if ($userAction != "Category") {
+		if ($link->query($sql)) {
+			// good
+		} else {
+			// Error
+			$link->close();
+			die ("There was an error inserting data into the DB from the CSV file.");
+                }
+	}
+}
+fclose($csv) or die("can't close file");
+
+
+?>
