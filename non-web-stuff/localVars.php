4a71100edcc24f3dcb34f97637bf282101db16b5
wayne.workman2012@gmal.com <wayne.workman2012@gmail.com>
2016-04-08 11:54:58 -0500

    Minor adjustments.

--- non-web-stuff/localVars.php
+++ non-web-stuff/localVars.php
@@ -6,11 +6,12 @@ $username = 'janeSystem';
 $password = 'janesystempassword';
 $database = 'jane';
 $PathToSMBShares = '/jane/';
-$PathToCSV = '/root/testdata.csv';
+$PathToCSV = '/jane/FF_AddsDropsFile.csv';
 $SMB_TO_USE="/etc/samba/smb.conf";
 $tmpFile = "$SMB_TO_USE.tmp";
 $ImportIP = "10.51.1.85";
 $SMB_SLEEP_TIME = "3600"; // 1 hour.
 $JaneEngine_Sleep_Time = "86400"; // 24 hours.
+
 ?>
 
