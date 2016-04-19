6f398494c973ad46f0839abb21884db411d8f313
wayne.workman2012@gmal.com <wayne.workman2012@gmail.com>
2016-04-19 15:46:28 -0500

    Set default JaneEngine sleep time to 15 minutes.

--- non-web-stuff/localVars.php
+++ non-web-stuff/localVars.php
@@ -11,7 +11,7 @@ $SMB_TO_USE="/etc/samba/smb.conf";
 $tmpFile = "$SMB_TO_USE.tmp";
 $ImportIP = "10.51.1.85";
 $SMB_SLEEP_TIME = "3600"; // 1 hour.
-$JaneEngine_Sleep_Time = "180"; // 24 hours is 86400 and is default. For testing, 3 minutes can be used without issue. 180 seconds.
+$JaneEngine_Sleep_Time = "900"; // 24 hours is 86400 and is default. For testing, 3 minutes can be used without issue. 180 seconds.
 
 ?>
 
