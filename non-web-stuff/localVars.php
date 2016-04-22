82ed658f95d51cf23350c5d8a93a621a7b79cda5
wayne.workman2012@gmal.com <wayne.workman2012@gmail.com>
2016-04-22 09:44:16 -0500

    Changed variable and comments for sleep time.

--- non-web-stuff/localVars.php
+++ non-web-stuff/localVars.php
@@ -10,7 +10,7 @@ $PathToCSV = '/jane/imports/FF_AddsDrops.csv';
 $SMB_TO_USE="/etc/samba/smb.conf";
 $tmpFile = "$SMB_TO_USE.tmp";
 $ImportIP = "10.51.1.85";
-$JaneEngine_Sleep_Time = "900"; // 24 hours is 86400 and is default. For testing, 3 minutes can be used without issue. 180 seconds.
+$JaneEngine_Sleep_Time = "300"; // 24 hours is 86400 seconds. For testing, 3 minutes can be used without issue, 180 seconds. For most purposes, every 7, 14, or 17 minutes would be fine.
 
 ?>
 
