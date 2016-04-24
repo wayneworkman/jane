f74e9301bf737311fcf2102bde0bd013a61922e2
wayne.workman2012 <wayne.workman2012@gmail.com>
2016-04-23 19:30:28 -0500

    Beginings of simplifying admin tasks.

--- jane.php
+++ jane.php
@@ -115,6 +115,63 @@ if ($SessionIsVerified == "1") {
 
 	if ($isAdministrator == 1) {
 
+
+
+		
+
+
+		echo "<div>";
+		echo "<form action=\"AdminAction.php\" method=\"post\">";
+		echo "User, Group, and IP Management<br>";
+		echo "<p class=\"tab\">";
+
+		
+		echo "Text for selected action:<br><input type=\"text\" name=\"adminActionText\"><br>";
+		echo "<br>";
+	
+		echo "<select name='uID'>";
+		echo "<option value=''>Pick User</option>";
+		$sql = "SELECT JaneUsername,JaneUserID from janeUsers";
+		$result = $link->query($sql);
+		if ($result->num_rows > 0) {
+			while($row = $result->fetch_assoc()) {
+				echo "<option value='" . trim($row['JaneUserID']) . "'>" . trim($row['JaneUsername']) . "</option>";
+			}
+			
+		} else {
+			echo "<option value='no_users'>no_users</option>";
+		}
+		echo "</select>";
+
+		echo "<select name='gID'>";
+		echo "<option value=''>Pick Group</option>";
+		$sql = "SELECT JaneGroupID,JaneGroupName from janeGroups";
+		$result = $link->query($sql);
+		if ($result->num_rows > 0) {
+			while($row = $result->fetch_assoc()) {
+				echo "<option value='" . trim($row['JaneGroupID']) . "'>" . trim($row['JaneGroupName']) . "</option>";
+			}
+		} else {
+			echo "<option value='no_groups'>no_settings</option>";
+		}
+		echo "</select><br>";
+		echo "<br>";
+		
+		$i = 0;
+		foreach ($adminActionNames as $adminAction) {
+			echo "<input type=\"radio\" name=\"adminAction\" value=\"$i\">$adminAction<br>";
+			$i = $i + 1;
+		}
+
+		echo "<br>";
+		echo "<input type=\"submit\">";
+		echo "</form>";
+		echo "</div>";
+
+
+
+
+
 		echo "<div>";
                 echo "<form action=\"NewSettings.php\" method=\"post\">";
                 echo "New Settings<br>";
--- vars.php
+++ vars.php
@@ -10,5 +10,20 @@ $PasswordDefault = "changeme";
 $SiteErrorMessage = "ERROR: There was a site problem. Report this to your Jane administrator and try again later.";
 $BadLoginError = "ERROR: Username and/or password is no good.";
 $IPBlockedMessage = "ERROR: You are blocked. Report this to your Jane administrator and try again later.";
+
+$adminActionNames = array();
+$adminActionNames[] = "Add New User";
+$adminActionNames[] = "Delete Selected User";
+$adminActionNames[] = "Enable Selected User";
+$adminActionNames[] = "Disable Selected User";
+$adminActionNames[] = "Reset Selected Users Jane Password to \"$PasswordDefault\"";
+$adminActionNames[] = "Reset Selected Users SMB Password to \"$PasswordDefault\"";
+$adminActionNames[] = "Create New Group";
+$adminActionNames[] = "Delete Selected Group";
+$adminActionNames[] = "Add Selected User To Selected Group";
+$adminActionNames[] = "Remove Selected User From Selected Group";
+
+
+
 ?>
 
