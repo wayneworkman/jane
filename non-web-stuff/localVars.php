b69088d6847142cdd7fcde65be33894aeecc7bb3
wayne.workman2012 <wayne.workman2012@gmail.com>
2016-05-09 18:59:50 -0500

    Set timezone for backend purposes.

--- non-web-stuff/localVars.php
+++ non-web-stuff/localVars.php
@@ -11,6 +11,11 @@ $SMB_TO_USE="/etc/samba/smb.conf";
 $tmpFile = "$SMB_TO_USE.tmp";
 $ImportIP = "10.51.1.85";
 $JaneEngine_Sleep_Time = "300"; // 24 hours is 86400 seconds. For testing, 3 minutes can be used without issue, 180 seconds. For most purposes, every 7, 14, or 17 minutes would be fine.
+$TimeZone="America/Chicago";
+//Find a listing of timezones here:
+//  http://php.net/manual/en/timezones.php
+date_default_timezone_set($TimeZone);
+
 
 ?>
 
