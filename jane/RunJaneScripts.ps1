0eb0b60a8857bffeaef6e0865374cd707758532b
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2016-08-03 10:25:34 -0500

    Updated automation example.

--- jane/RunJaneScripts.ps1
+++ jane/RunJaneScripts.ps1
@@ -13,13 +13,10 @@ $file1 = "Z:\Created-" + $(Get-Date -Format yyyy-MM-dd) + "---abc.ps1"
 #File 2 location:
 $file2 = "Z:\Created-" + $(Get-Date -Format yyyy-MM-dd) + "---def.ps1"
 
-
-
-
 #Check if file1 exists. If so, run it and redirect output to log, then delete file1 and file1.signed
 $FileExists = Test-Path $file1
 If ($FileExists -eq $True) {
-    powershell.exe -executionpolicy bypass -file $file1 2>&1 | Out-File $log -Append
+    powershell.exe -executionpolicy bypass -windowstyle hidden -noninteractive -nologo -file $file1 2>&1 | Out-File $log -Append
     Remove-Item $file1
     $file1 = $file1 + ".signed"
     $FileExists = Test-Path $file1
@@ -31,7 +28,7 @@ If ($FileExists -eq $True) {
 #Check if file2 exists. If so, run it and redirect output to log, then delete file2 and file2.signed
 $FileExists = Test-Path $file2
 If ($FileExists -eq $True) {
-    powershell.exe -executionpolicy bypass -file $file2 2>&1 | Out-File $log -Append
+    powershell.exe -executionpolicy bypass -windowstyle hidden -noninteractive -nologo -file $file2 2>&1 | Out-File $log -Append
     Remove-Item $file2
     $file2 = $file2 + ".signed"
     $FileExists = Test-Path $file2
