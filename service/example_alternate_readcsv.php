e1f812cc56ed5d6382d4a5faa014276c0bc3de72
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2016-08-06 12:23:58 -0500

    Made corrections and notes to example readcsv

--- service/example_alternate_readcsv.php
+++ service/example_alternate_readcsv.php
@@ -9,7 +9,11 @@ $symbols = array();
 $symbols += range('a', 'z');
 $symbols += range('A', 'Z');
 $symbols += range('0', '9');
-array_push($symbols,' ','-',','); // Allow spaces and hyphens and commas in this example, because we perform data sterilizing on the csv line itself.
+array_push($symbols,' ','-'); // Allow spaces and hyphens.
+
+
+//Values that are not present in the import file should be loaded with a space.
+$userMiddleName = " ";
 
 
 
@@ -17,15 +21,18 @@ if (file_exists($PathToCSV)) {
 	$csv = fopen($PathToCSV,'r') or die("can't open file");
 	while($csv_line = fgetcsv($csv)) {
 
-		
-		//Clean entire line at once:
-                $csv_line = trim(preg_replace("/[^" . preg_quote(implode('',$symbols), '/') . "]/i", "", $csv_line));
-
 
 
 		list($userImportedID, $NotSupportedAuth, $userUserName, $userPassword, $NotSupportedEmail, $userFirstName, $userLastName, $NotSupportedCity, $NotSupportedCountry, $NotSupportedLanguage, $NotSupportedInstitution, $NotSupportedDepartment, $userAction, $NotSupportedProfileFieldRelation, $userGroup, $NotSupportedProfileFieldHomeroom, $NotSupportedProfileFieldTeam, $NotSupportedProfileFieldGrade, $NotSupportedProfileFieldStatus) = $csv_line;
 
+		$userAction = trim(preg_replace("/[^" . preg_quote(implode('',$symbols), '/') . "]/i", "", $userAction));
+		$userFirstName = trim(preg_replace("/[^" . preg_quote(implode('',$symbols), '/') . "]/i", "", $userFirstName));
+		$userMiddleName = trim(preg_replace("/[^" . preg_quote(implode('',$symbols), '/') . "]/i", "", $userMiddleName));
+		$userLastName = trim(preg_replace("/[^" . preg_quote(implode('',$symbols), '/') . "]/i", "", $userLastName));
+		$userGroup = trim(preg_replace("/[^" . preg_quote(implode('',$symbols), '/') . "]/i", "", $userGroup));
+		$userUserName = trim(preg_replace("/[^" . preg_quote(implode('',$symbols), '/') . "]/i", "", $userUserName));
 
+// fields containing only numeric values are being completely removed. Need to fix, for now don't clense them.
 
 		//Ensure every single username is unique.
 		//If an abnormal username must be given, log it in the DB, and list abnormal usernames and IDs in the web UI for all to see.
@@ -34,7 +41,7 @@ if (file_exists($PathToCSV)) {
 
 
 		$sql="INSERT INTO userDataToImport (userAction,userFirstName,userMiddleName,userLastName,userGroup,userUserName,userPassword,userImportedID) VALUES ('$userAction','$userFirstName','$userMiddleName','$userLastName','$userGroup','$userUserName','$userPassword','$userImportedID')";
-		if ($userAction != "Category") { //this check is how the header line is skipped. "Catagory" can be replaced with a word that always appears in the header.
+		if ($userAction != "idnumber") { //this check is how the header line is skipped. "Catagory" can be replaced with a word that always appears in the header.
 			if ($link->query($sql)) {
 				// good
 			} else {
