0515897f21061ea0adae6b9bb93d2a37c862ee06
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2016-08-09 20:04:04 -0500

    Updated example custom readcsv.php file.

--- service/custom_readcsv.php
+++ service/custom_readcsv.php
@@ -1,8 +1,5 @@
 <?php
 
-// To use this file, after installation, replace 'readcsv.php' inside /jane/service/JaneEngine.php with this file's name.
-// The line is towards the top.
-
 
 //Allowed characters within user data:
 $symbols = array();
@@ -15,14 +12,27 @@ array_push($symbols,' ','-'); // Allow spaces and hyphens.
 //Values that are not present in the import file should be loaded with a space.
 $userMiddleName = " ";
 
+while (file_exists($PathToCSV)) {
+	$rightNow = new DateTime("@" . time());
+	$rightNow->setTimezone(new DateTimeZone("$TimeZone"));
+	echo $rightNow->format("F j, Y, g:i a") . " Import file found at: '$PathToCSV'\n";
 
 
-if (file_exists($PathToCSV)) {
-	$csv = fopen($PathToCSV,'r') or die("can't open file");
+	$isFileClosed = exec("lsof -- '$PathToCSV';echo $?");
+	if ($isFileClosed != "1") {
+		$rightNow = new DateTime("@" . time());
+		$rightNow->setTimezone(new DateTimeZone("$TimeZone"));
+		echo $rightNow->format("F j, Y, g:i a") . " Import file is still open, delaying until next iteration.\n";
+		break;
+	}
+
+
+	$csv = fopen($PathToCSV,'r') or die(" Failed to open file '$PathToCSV'\n");
+	$rightNow = new DateTime("@" . time());
+	$rightNow->setTimezone(new DateTimeZone("$TimeZone"));
+	echo $rightNow->format("F j, Y, g:i a") . " Successfully opened File: '$PathToCSV'\n";
+
 	while($csv_line = fgetcsv($csv)) {
-
-
-
 		list($userImportedID, $NotSupportedAuth, $userUserName, $userPassword, $NotSupportedEmail, $userFirstName, $userLastName, $NotSupportedCity, $NotSupportedCountry, $NotSupportedLanguage, $NotSupportedInstitution, $NotSupportedDepartment, $userAction, $NotSupportedProfileFieldRelation, $userGroup, $NotSupportedProfileFieldHomeroom, $NotSupportedProfileFieldTeam, $NotSupportedProfileFieldGrade, $NotSupportedProfileFieldStatus) = $csv_line;
 
 		$userAction = trim(preg_replace("/[^" . preg_quote(implode('',$symbols), '/') . "]/i", "", $userAction));
@@ -32,11 +42,13 @@ if (file_exists($PathToCSV)) {
 		$userGroup = trim(preg_replace("/[^" . preg_quote(implode('',$symbols), '/') . "]/i", "", $userGroup));
 		$userUserName = trim(preg_replace("/[^" . preg_quote(implode('',$symbols), '/') . "]/i", "", $userUserName));
 
-// fields containing only numeric values are being completely removed. Need to fix, for now don't clense them.
+
 
 		//Ensure every single username is unique.
 		//If an abnormal username must be given, log it in the DB, and list abnormal usernames and IDs in the web UI for all to see.
                 $isAbnormal="0"; //Initially set abnormal to false.
+                //The below file is what ensures unique usernames. 
+                //You may comment this line to disable this functionality but doing so would risk duplicate usernames and errors.
                 include 'ensureUniqueUsernames.php';
 
 
@@ -47,11 +59,11 @@ if (file_exists($PathToCSV)) {
 			} else {
 				// Error
 				$link->close();
-				die ("There was an error inserting data into the DB from the CSV file. SQL query was:\n\n$sql\n\n");
+				die (" There was an error inserting data into the DB from the CSV file. SQL query was:\n\n$sql\n\n");
 			}
 		}
 	}
-	fclose($csv) or die("can't close file");
+	fclose($csv) or die(" Can't close file '$PathToCSV'\n");
 	// below line sets aside old import files, can be commented out to not preserve incoming data.
 	//copy($PathToCSV, $PathToCSV . "." . date('Y-m-d'));
 	// below line deletes current import file.
