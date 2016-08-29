5df6f6d4deb0bbd65fd863a646fb2464e80cc076
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2016-08-29 10:34:54 -0500

    Added bad login clearing.

--- jane/BadLoginAttempts.php
+++ jane/BadLoginAttempts.php
@@ -8,10 +8,20 @@ if ($SessionIsVerified == "1") {
 		echo "<title>Bad Login Attempts</title>";
 		echo "<div>";
 		//Recent bad login attempts.
-		echo "<font color=\"red\">Recent bad login attempts:</font><br><br>";
-		$sql = "SELECT `badREQUEST_TIME`,`badUsername`,`badREMOTE_ADDR`,`badHTTP_USER_AGENT` FROM `badLoginAttempts` ORDER BY `badREQUEST_TIME` desc LIMIT 40";
+		echo "<font color=\"red\">Bad login attempts</font><br><br>";
+
+		$sql = "SELECT `badREQUEST_TIME`,`badUsername`,`badREMOTE_ADDR`,`badHTTP_USER_AGENT` FROM `badLoginAttempts` ORDER BY `badREQUEST_TIME` desc";
 		$result = $link->query($sql);
 		if ($result->num_rows > 0) {
+
+
+			echo "<form action=\"clearBadLogins.php\" method=\"post\">\n";
+			echo "Clear bad login attempts older than\n";
+			echo " <input type=\"text\" name=\"years\" value=\"1\" style=\"width:20px;\"> years.<br>\n";
+			echo "<input type=\"checkbox\" name=\"ConfirmDelete\" value=\"Confirmed\">Confirm Delete<br>\n";
+			echo "<input type=\"submit\" value=\"Clear Bad Logins\">\n";
+			echo "<br><br>\n";
+
 			echo "<table>\n";
 			echo "<tr>\n";
 			echo "<th>Time</th>\n";
---
+++ jane/clearBadLogins.php
@@ -0,0 +1,39 @@
+<?php
+include 'vars.php';
+include 'verifysession.php';
+if ($SessionIsVerified == "1") {
+	include 'connect2db.php';
+	if ($isAdministrator == 1) {
+
+		if (isset($_REQUEST['ConfirmDelete'])) {
+			$ConfirmDelete = $link->real_escape_string(trim($_REQUEST['ConfirmDelete']));
+		}
+		$years = $link->real_escape_string(trim($_REQUEST['years']));
+		$thisMoment = time();
+
+		if ((is_numeric($years)) && ($ConfirmDelete == "Confirmed")) {
+			$timeDifference = ( $thisMoment - ($years * 31536000)); // 1 year in seconds is 31536000.
+
+			$sql = "DELETE FROM `badLoginAttempts` WHERE `badREQUEST_TIME` <= '$timeDifference'";
+			if ($link->query($sql)) {
+				// good, send back to usernameTracking.
+				$NextURL="BadLoginAttempts.php";
+				header("Location: $NextURL");
+			} else {
+				// Error
+				$link->close();
+				die ($SiteErrorMessage);
+			}
+		} else {
+			$link->close();
+			die ($invalidData);
+		}
+
+
+	} else {
+		//Not an admin, redirect to home.
+		$NextURL="jane.php";
+		header("Location: $NextURL");
+	}
+}
+?>
