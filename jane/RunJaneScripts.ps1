d771ef36a15b90b1aa830dfbbf2b012e5b23247a
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2016-09-07 11:22:38 -0500

    New powershell file to hopefully solve mapped drive connectivity issues.

--- jane/RunJaneScripts.ps1
+++ jane/RunJaneScripts.ps1
@@ -1,44 +1,82 @@
 #This file should be stored locally on the server that will execute the import scripts,
 #and scheduled to run however often is needed. 
-#Paths and filenames need adjusted apropriately.
-#
-#Redirecting the output to a log file is optional but can help with diagnosing issues.
+#Paths and filenames need adjusted apropriately for your setup.
+#Adjust the below variables as needed, don't bother below the line unless you have strong scripting knowedge.
 
-#Log location:
-$log = "z:\Jane.log"
+#Drive letter for mapped drive to use, J for Jane, but you may use whatever letter available.
+$driveLetter="J"
 
-#File 1 location:
-$file1 = "Z:\Created-" + $(Get-Date -Format yyyy-MM-dd) + "---abc.ps1"
+#Your Jane username:
+$janeUsername="wayne"
 
-#File 2 location:
-$file2 = "Z:\Created-" + $(Get-Date -Format yyyy-MM-dd) + "---def.ps1"
+#Your Jane SMB password:
+$janeSMBPassword="MyAwesomePassword"
 
-#Check if file exists. If so, run it and redirect output to log, then delete file and file.signed
-$currentFile = $file1
-$FileExists = Test-Path $currentFile
-If ($FileExists -eq $True) {
-    powershell.exe -executionpolicy bypass -windowstyle hidden -noninteractive -nologo -file $currentFile 2>&1 | Out-File $log -Append
-    Remove-Item $currentFile
-    $currentFile = $currentFile + ".signed"
-    $FileExists = Test-Path $currentFile
+#Optional domain, I don't think Samba really cares about this with how Jane uses Samba, but you may want it:
+$domainName="jane"
+
+#Jane settings group name. 
+#This is the group name that the settings sets belong to, and that you belong to.
+#Groups are managed in the web interface. You must be a member of the setting's group
+#in order to access it's scripts.
+$janeGroupName="testGroup"
+
+#Jane server hostname or IP address:
+$janeHostname="10.0.0.12"
+
+#Jane settings Nicknames. This is a list of all NickNames for Jane Settings-sets you want
+#to run using this particular script, on this particular server.
+#Entries are double-quoted, and comma seperated as shown below in the sample. 
+#The below uses Jane Setting nick names abc and def.
+#These nicnames are defined within the web interface, and are case sensitive.
+#This list can be as long as it needs to be.
+$nickNames= @("abc","def")
+
+#Log location, this is where you want the log to be, and what it should be named.
+
+#Storing on the mapped drive:
+#$log = $driveLetter + ":\Jane.log"
+#Storing on the C:\ drive:
+$log = "C:\Jane.log"
+
+
+########################################################################################################
+# Do not modify below here unless you know what you're doing - or aren't worried about breaking things.#
+########################################################################################################
+
+#Here, check if the drive letter is already in use. If so, delete it.
+if (!(Get-PSDrive $driveLetter)) {New-PSDrive -Name $driveLetter -PSProvider Registry -Root HKLM:\Network}else { 
+    Get-PSDrive $driveLetter | Remove-PSDrive
+}
+
+#Map the Jane share using the defined settings above.
+$pass=$janeSMBPassword|ConvertTo-SecureString -AsPlainText -Force
+$Cred = New-Object System.Management.Automation.PsCredential($janeUsername + "@" + $domainName,$pass)
+New-PSDrive -name $driveLetter -Root \\$janeHostname\$janeGroupName -Credential $cred -PSProvider filesystem
+
+#Begin loop for jane settings nick names. This loops through each one.
+for ($i=0; $i -lt $nickNames.length; $i++) {
+
+    #Build the filename.
+    $file = $driveLetter + ":\Created-" + $(Get-Date -Format yyyy-MM-dd) + "---" + $nickNames[$i] + ".ps1"
+
+    #Check if file exists. If so, run it and redirect output to log, then delete file and file.signed
+    $FileExists = Test-Path $file
     If ($FileExists -eq $True) {
-        Remove-Item $currentFile
+        powershell.exe -executionpolicy bypass -windowstyle hidden -noninteractive -nologo -file $file 2>&1 | Out-File $log -Append
+        Remove-Item $file
+        $currentFile = $file + ".signed"
+        $FileExists = Test-Path $file
+        If ($FileExists -eq $True) {
+            Remove-Item $file
+        }
     }
+#end loop
 }
 
 
-
-$currentFile = $file2
-$FileExists = Test-Path $currentFile
-If ($FileExists -eq $True) {
-    powershell.exe -executionpolicy bypass -windowstyle hidden -noninteractive -nologo -file $currentFile 2>&1 | Out-File $log -Append
-    Remove-Item $currentFile
-    $currentFile = $currentFile + ".signed"
-    $FileExists = Test-Path $currentFile
-    If ($FileExists -eq $True) {
-        Remove-Item $currentFile
-    }
+#Cleanup the share.
+if (!(Get-PSDrive $driveLetter)) {New-PSDrive -Name $driveLetter -PSProvider Registry -Root HKLM:\Network}else {
+    Get-PSDrive $driveLetter | Remove-PSDrive
 }
-
-
 
