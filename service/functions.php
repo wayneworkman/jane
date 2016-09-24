6092e08a92bc8a422ef001c8e24544476749b1d8
wayne.workman2012@gmail.com <wayne.workman2012@gmail.com>
2016-09-24 10:50:06 -0500

    Fixed error with logs, reformatted janeADEngine file.

--- service/functions.php
+++ service/functions.php
@@ -3,6 +3,7 @@
 
 //Write message to log.
 function writeLog($theMessage) {
+	global $TimeZone;
 	$rightNow = new DateTime("@" . time());
 	$rightNow->setTimezone(new DateTimeZone("$TimeZone"));
 	echo $rightNow->format("F j, Y, g:i a") . " $theMessage\n";
